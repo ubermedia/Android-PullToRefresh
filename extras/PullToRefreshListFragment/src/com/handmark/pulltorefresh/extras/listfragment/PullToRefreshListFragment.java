46581d82c0a46cdbe4a6db4daaf14f659f232097
Vladimir Berbenchuk <vladimir.berbenchuk@postindustria.com>
2020-01-14 01:53:08 +0200

    Migration to androidx

--- extras/PullToRefreshListFragment/src/com/handmark/pulltorefresh/extras/listfragment/PullToRefreshListFragment.java
+++ extras/PullToRefreshListFragment/src/com/handmark/pulltorefresh/extras/listfragment/PullToRefreshListFragment.java
@@ -16,7 +16,7 @@
 package com.handmark.pulltorefresh.extras.listfragment;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import androidx.fragment.app.ListFragment;
 import android.view.LayoutInflater;
 
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
--- sample/src/com/handmark/pulltorefresh/samples/PullToRefreshListFragmentActivity.java
+++ sample/src/com/handmark/pulltorefresh/samples/PullToRefreshListFragmentActivity.java
@@ -20,7 +20,7 @@ import java.util.LinkedList;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
